d44fcf5f24b0c591af97c35e632f3fda2b77b976
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2016-06-28 16:19:32 +0100

    Add auth token and url in service creation

--- sd.js
+++ sd.js
@@ -1,8 +1,8 @@
 var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
 
-
 // Service Discovery Configurations
+sdService = appEnv.getService("myServiceDiscovery");
 var ttl = 300;
 var serviceName = "Catalog";
 var serviceHost = appEnv.bind;
@@ -10,7 +10,7 @@ var servicePort = "http"
 var serviceUrl = appEnv.url
 
 var ServiceDiscovery = require('bluemix-service-discovery');
-var discovery = new ServiceDiscovery({ name: 'myServiceDiscovery', version: 1 });
+var discovery = new ServiceDiscovery({ name: 'myServiceDiscovery', auth_token: sdService.credentials.auth_token, url: sdService.credentials.url, version: 1 });
 
 // register a service and send heartbeats
 discovery.register({
